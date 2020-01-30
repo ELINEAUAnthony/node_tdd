4646609df13a8c0852ce3f37f8b0df4977ccd4c9
Anthony <anthony.elineau@hotmail.fr>
2020-01-30 13:55:55 +0100

    mongoose connection

--- models/index.js
+++ models/index.js
@@ -1,42 +1,17 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 console.log(env);
 const config = require(__dirname + '/../config/config');
-const db = {};
+const db = require('mongoose');
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.connect(`mongodb://localhost:27017/${config.data}`, (err) => {
+  if (err) throw err
+  console.log('mongodb successfully connected.')
+})
 
 db.close = async () => {
-  await db.sequelize.close()
+  await db.connection.close()
 };
 
 module.exports = db;
